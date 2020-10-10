a7cc4251f1505587c80c84604d4485eaeb485f60
Leperkhanh <jordannguyen880@gmail.com>
2020-10-09 20:49:45 -0400

    update new, create, show for controller and views

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,14 +1,21 @@
 class HostsController < ApplicationController
   def index
+    @hosts = Host.all
   end
 
   def show
+    @host = Host.find_by(id: params[:id])
   end
 
   def new
+    @host = Host.new
   end
 
   def create
+    @host = Host.new(host_params)
+      if @host.save
+        redirect_to host_path(@host)
+      end
   end
 
   def edit
@@ -18,6 +25,12 @@ class HostsController < ApplicationController
   end
 
   def destroy 
-    
+
+  end
+
+  private 
+
+  def host_params
+    params.require(:host).permit(:email, :first_name, :last_name, :phone_number, :bio, :password)
   end
 end
